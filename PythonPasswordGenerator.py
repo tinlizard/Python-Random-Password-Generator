a0c2a981b1563010a66d04f1bb498c621031a4b2
tinlizard <38041074+tinlizard@users.noreply.github.com>
2018-05-06 11:52:37 +0200

    Create PythonPasswordGenerator.py
    Python secure password generator file

---
+++ PythonPasswordGenerator.py
@@ -0,0 +1,13 @@
+import random
+import time
+
+def passwordGenerator():
+    integer = ['12', '26', '45', '321', '187', '92', '451', '877']
+    string = ['ArtilerrY','TONgue','Persson','AppLe','Orrange','Magg','LiFe', 'Arrgon','Kryyptonite','MarVel','Universe','Acccellerate']
+
+    print("Generating password, please wait.")
+    time.sleep(1)
+    print(random.choice(string) + random.choice(integer))
+
+passwordGenerator()
+    
