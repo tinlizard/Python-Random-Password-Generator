484cf04b5b71601c77a4ceb3eceefe308c9f0281
tinlizard <38041074+tinlizard@users.noreply.github.com>
2018-05-10 16:27:11 +0200

    Update PythonPasswordGenerator.py

--- PythonPasswordGenerator.py
+++ PythonPasswordGenerator.py
@@ -4,10 +4,11 @@ import time
 def passwordGenerator():
     integer = ['12', '26', '45', '321', '187', '92', '451', '877']
     string = ['ArtilerrY','TONgue','Persson','AppLe','Orrange','Magg','LiFe', 'Arrgon','Kryyptonite','MarVel','Universe','Acccellerate']
-
+    string2 = ['Amon', 'Salmon', 'Cheese', 'Omano', 'Tiger', 'Giraffe', 'Euro', 'Lego']
+    
     print("Generating password, please wait.")
     time.sleep(1)
-    print(random.choice(string) + random.choice(integer))
+    print(random.choice(string) + random.choice(string2) + random.choice(integer))
 
 passwordGenerator()
     
